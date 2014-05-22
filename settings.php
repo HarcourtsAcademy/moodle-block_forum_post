17d8229a88370b25dcc395b391391ae757773489
Tim Butler <timb20@gmail.com>
2014-05-22 14:15:04 +1000

    Initial commit.

---
+++ settings.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Block for posting to a course forum
+ *
+ * @package    block_forum_post
+ * @copyright  Tim Butler <tim.butler@harcourts.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+//$settings->add(new admin_setting_heading('sampleheader',
+//                                         get_string('headerconfig', 'block_forum_post'),
+//                                         get_string('descconfig', 'block_forum_post')));
+//
+//$settings->add(new admin_setting_configcheckbox('forum_post/foo',
+//                                                get_string('labelfoo', 'block_forum_post'),
+//                                                get_string('descfoo', 'block_forum_post'),
+//                                                '0'));
